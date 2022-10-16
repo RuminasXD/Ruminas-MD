7b85303fb72491f1e94c7f2c9b85972b14f55ace
RuminasXD <115128054+RuminasXD@users.noreply.github.com>
2022-10-16 22:42:42 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,8 +1,36 @@
-function handler(m) {
-  let data = global.owner.filter(([id, isCreator]) => id && isCreator)
-  this.sendContact(m.chat, data.map(([id, name]) => [id, name]), m)
+import fetch from 'node-fetch'
+let handler = async (m, { conn, usedPrefix, text, args, command }) => {
+let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+let pp = await conn.profilePictureUrl(who).catch(_ => hwaifu.getRandom())
+let name = await conn.getName(who)
+
+
+if (command == 'owner') {
+ let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:WhatsApp;Kyouka Hashiba Owner;Sakura;;Md\nFN:Nacht Owner Sakura, Md\nNICKNAME:👑 Owner Sakura Bot\nORG:Nacht\nTITLE:hashiba\nitem1.TEL;waid=62895627514070:+62 895-6275-14070\nitem1.X-ABLabel:📞 Nomor Owner\nitem2.URL:https://nacht-website.vercel.app/\nitem2.X-ABLabel:💬 More\nitem3.EMAIL;type=INTERNET:gaadaemailpribadi@gmail.com\nitem3.X-ABLabel:💌 Mail Owner SakuraBot\nitem4.ADR:;;🇮🇩 Indonesia;;;;\nitem4.X-ABADR:💬 More\nitem4.X-ABLabel:📍 Lokasi Saya\nBDAY;value=date:🔖 16 Tahun\nEND:VCARD`
+const tag_own = await conn.sendMessage(m.chat, { contacts: { displayName: wm, contacts: [{ vcard }] }}, { quoted: fliveLoc })
+let caption = `👋 Hai *${name} @${who.split("@")[0]}*, Nih Owner *${conn.user.name}* kak`
+    await conn.sendButton(m.chat, caption, author, null, [['🎀 Sapa Owner', 'Huuu']], m, { quoted: tag_own, mentions: conn.parseMention(caption) })
 }
-handler.alias = ['owner']
-handler.command = /^(owner)$/i
+if (command == 'pengembang') {
+  let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;${author};;;\nFN:${author}\nORG:${author}\nTITLE:\nitem1.TEL;waid=62895627514070:+62 895-6275-14070\nitem1.X-ABLabel:${author}\nX-WA-BIZ-DESCRIPTION:${htjava} Nih pengembang ku kack yg mengaktifkan bot ini.\nX-WA-BIZ-NAME:${author}\nEND:VCARD`
+await conn.sendMessage(m.chat, { contacts: { displayName: wm, contacts: [{ vcard }] }}, { quoted: fgif })
+}
+if (command == 'creator') {
+  try {
+  const sentMsg = await conn.sendContactArray(m.chat, [
+    [`${nomorown}`, `${await conn.getName(nomorown+'@s.whatsapp.net')}`, `👑 Developer Bot `, `🚫 Don't call me 🥺`, `wudysoft@gmail.com`, `🇮🇩 Indonesia`, `🚀 Nacht`, `bimlek`],
+    [`${conn.user.jid.split('@')[0]}`, `${await conn.getName(conn.user.jid)}`, `🔥 Bot WhatsApp 🐣`, `📵 Don't spam/call me 😢`, `Nothing`, `🇮🇩 Indonesia`, `🚀 https://s.id/Cerdasin62/`, `🤖 Hanya bot biasa yang kadang suka eror ☺`]
+  ], fkontak)
+  await conn.reply(m.chat, `Halo kak @${m.sender.split(`@`)[0]} itu nomor ownerku , jangan di spam ya ka😉`, sentMsg, {mentions: [m.sender]})
+  } catch {
+  const sentMsg = await conn.sendContact(m.chat, `${nomorown}`, `${await conn.getName(nomorown+'@s.whatsapp.net')}`, m)
+  await conn.reply(m.chat, `Halo kak @${m.sender.split(`@`)[0]} itu nomor team developerku, jangan di apa-apain ya kak😖`, sentMsg, {mentions: [m.sender]})
+  }
+  }
+}
+handler.help = ['owner', 'creator']
+handler.tags = ['info']
+
+handler.command = /^(owner|pengembang|creator)$/i
 
 export default handler
