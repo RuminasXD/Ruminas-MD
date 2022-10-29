b714db66f4c749a543c67e7c40bb54f801503515
RuminasXD <115128054+RuminasXD@users.noreply.github.com>
2022-10-29 09:38:39 +0700

    Update z-aselole.js

--- plugins/z-aselole.js
+++ plugins/z-aselole.js
@@ -6,7 +6,7 @@ let name = await conn.getName(who)
 
 
 if (command == 'owner') {
- let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:WhatsApp;Kyouka Hashiba Owner;Sakura;;Md\nFN:Nacht Owner Sakura MD\nNICKNAME:👑 Owner Sakura Bot\nORG:Nacht\nTITLE:hashiba\nitem1.TEL;waid=62895627514070:+62 895-6275-14070\nitem1.X-ABLabel:📞 Nomor Owner\nitem2.URL:https://nacht-website.vercel.app/\nitem2.X-ABLabel:💬 More\nitem3.EMAIL;type=INTERNET:gaadaemailpribadi@gmail.com\nitem3.X-ABLabel:💌 Mail Owner SakuraBot\nitem4.ADR:;;🇮🇩 Indonesia;;;;\nitem4.X-ABADR:💬 More\nitem4.X-ABLabel:📍 Lokasi Saya\nBDAY;value=date:🔖 16 Tahun\nEND:VCARD`
+ let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:WhatsApp;Kyouka Hashiba Owner;Sakura;;Md\nFN:Ray\nNICKNAME:👑 Owner Sakura Bot\nORG:Nacht\nTITLE:hashiba\nitem1.TEL;waid=62895627514070:+62 895-6275-14070\nitem1.X-ABLabel:📞 Nomor Owner\nitem2.URL:https://nacht-website.vercel.app/\nitem2.X-ABLabel:💬 More\nitem3.EMAIL;type=INTERNET:gaadaemailpribadi@gmail.com\nitem3.X-ABLabel:💌 Mail Owner SakuraBot\nitem4.ADR:;;🇮🇩 Indonesia;;;;\nitem4.X-ABADR:💬 More\nitem4.X-ABLabel:📍 Lokasi Saya\nBDAY;value=date:🔖 16 Tahun\nEND:VCARD`
 const tag_own = await conn.sendMessage(m.chat, { contacts: { displayName: wm, contacts: [{ vcard }] }}, { quoted: fliveLoc })
 let caption = `👋 Hai *${name} @${who.split("@")[0]}*, Nih Owner *${conn.user.name}* kak`
     await conn.sendButton(m.chat, caption, author, null, [['🎀 Sapa Owner', 'Huuu']], m, { quoted: tag_own, mentions: conn.parseMention(caption) })
