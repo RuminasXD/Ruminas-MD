5dc75890ff5182b04bf4a638500bcc4ca4dcfc58
RuminasXD <115128054+RuminasXD@users.noreply.github.com>
2022-11-20 21:05:20 +0700

    Update intro.js

--- plugins/intro.js
+++ plugins/intro.js
@@ -37,11 +37,11 @@ let krtu = `0ཻུ۪۪ꦽꦼ̷⸙‹•═════════════�
 `
 let wibu = `https://api-reysekha.herokuapp.com/api/random/cosplay?apikey=apirey` 
 let thumb = await(await fetch(wibu)).buffer()
-conn.sendButtonDoc(m.chat, krtu, 'Script : https://youtube.com/channel/UCjoPsysjCn2Qa0dRalUb2mg\nSubs Tod!!','MENU','.menu', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: "https://youtube.com/channel/UCjoPsysjCn2Qa0dRalUb2mg",
-    mediaType: "VIDEO",
-    description: "https://youtube.com/channel/UCjoPsysjCn2Qa0dRalUb2mg", 
-    title: 'Al? Offc',
+conn.sendButtonDoc(m.chat, krtu, 'RimuruBot','MENU','.menu', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
+    mediaUrl: "github.com/RyhnXD",
+    mediaType: 2,
+    description: "github.com/RyhnXD", 
+    title: 'Rimuru Tempest',
     body: wm,
     thumbnail: thumb,
     sourceUrl: sgc
