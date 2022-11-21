d6b20704ce370e5be3becc7657250f5c8edffb47
RuminasXD <115128054+RuminasXD@users.noreply.github.com>
2022-11-21 16:25:36 +0700

    Update _autosticker.js

--- plugins/_autosticker.js
+++ plugins/_autosticker.js
@@ -7,23 +7,11 @@ let pp = await conn.profilePictureUrl(who).catch(_ => hwaifu.getRandom())
 let name = await conn.getName(who)
 
 let stiker = await sticker(null, global.API(`${pickRandom(stikerhuuu)}`), global.packname, global.author)
-    if (stiker) return await conn.sendFile(m.chat, stiker, 'sticker.webp', '', m, null, { fileLength: 100, contextInfo: {
-          externalAdReply :{
-          showAdAttribution: true,
-    mediaUrl: sgc,
-    mediaType: 2,
-    description: '𝑨𝒍𝒅𝒊 𝑿 𝑨𝒊𝒔𝒚𝒂𝒉', 
-    title: 'Hai, ' + name + ' ' + ucapan,
-    body: botdate,
-    thumbnail: await(await fetch(pp)).buffer(),
-    sourceUrl: sgc
-     }}
-  })
+    if (stiker) return await conn.sendFile(m.chat, stiker, 'sticker.webp', '', m, null, adReply)
     throw stiker.toString()
-   
 }
 
-handler.customPrefix = /^(hu|huu|huuu|huuuu|huuuuu|ok|oke|ook|okee|okk|nice|nis)$/i
+handler.customPrefix = /^(huuu)$/i
 handler.command = new RegExp
 
 export default handler
@@ -32,23 +20,14 @@ function pickRandom(list) {
   return list[Math.floor(list.length * Math.random())]
 }
 let stikerhuuu = [
- "https://telegra.ph/file/fa2bbea0f7de2575cf027.png",//patrick huu
- "https://telegra.ph/file/4a2db7bc9f3f9ecfc007d.png",//anime yntkts
- "https://telegra.ph/file/5f6079714851d9927697e.png",//windah bocil
- "https://telegra.ph/file/d5100b4ce95a0012e88c1.png",//patrick bawa minum
- "https://telegra.ph/file/2ade25087c89f86587853.png",//pak polisi pap tt
- "https://telegra.ph/file/eb2b5e5fff569896c1639.png",//kucing1
- "https://telegra.ph/file/bd8a0e7ea01218531798b.png",//kacamata
- "https://telegra.ph/file/300610838ffa0e6576eb9.png",//patrick pembohong
- "https://telegra.ph/file/954afe562e58c144620ae.png",//spongebob FBI
- "https://telegra.ph/file/72026dcc46e4cb4b6f9ae.png",//mazowski monster inc
- "https://telegra.ph/file/aa9f1bea869e362e6f56e.png",//wkwk
- "https://telegra.ph/file/09bbff0da316ba21b4f8e.png",//kucing2
- "https://telegra.ph/file/2e0637d57e3cc1abcb4a0.png",//patrick anak setan
- "https://telegra.ph/file/d771ae015b5486859d03f.png",//mazowski 2
- "https://telegra.ph/file/9c7606f571c05b4d0c941.png",//hengker
- "https://telegra.ph/file/84fd937257bcd614d6c9e.png",//anjing
- "https://telegra.ph/file/b8ba6989c00c50df049d0.png",//bapak bapak lovee
- "https://telegra.ph/file/2f618fffab6ff7bea32ab.png",//abang saleh
- "https://telegra.ph/file/dfbf483c209a31f01b4e5.png"//hengker2
+ "https://telegra.ph/file/70bdbcdccfb8b8fbfd6ac.png",//Ok Bang [ANM]
+ "https://telegra.ph/file/903dbd6f6190a7d068b0b.png",//mengsenyum [ANM]
+ "https://telegra.ph/file/971105914f40c0bed5e7a.png",//emuach [ANM]
+ "https://telegra.ph/file/6a182392daa480860955f.png",//Tumru [ANM]
+ "https://telegra.ph/file/f43c2067e10b19219a0da.png",//hutao kasih jempol [ANM]
+ "https://telegra.ph/file/c9efb557c4df3a4e97541.png",//bilek amat hidup :) [ANM]
+ "https://telegra.ph/file/0ecab7d01579895ce5c2c.png",//rimuru Â¹ [ANM]
+ "https://telegra.ph/file/0738f0bc1a6cc71b25374.png",//raiden ngambek [ANM]
+ "https://telegra.ph/file/a574d51acd213ad5f9018.png",//loli ngudud [ANM]
+ "https://telegra.ph/file/0589eaf2a29652b1b785d.png",//tatapan cintaðŸ—¿ [ANM]
 ]
